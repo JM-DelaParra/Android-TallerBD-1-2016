f46bf94f0fca3390b0fdab2db4462db42ea09de0
JM-DelaParra <jefferson.morales.inf@gmail.com>
2016-04-25 13:28:44 -0300

    base de datos app

--- AndroidBasic/app/src/main/java/com/citiaps/jefferson/androidbasic/MainActivity.java
+++ AndroidBasic/app/src/main/java/com/citiaps/jefferson/androidbasic/MainActivity.java
@@ -3,14 +3,27 @@ package com.citiaps.jefferson.androidbasic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "LifeCycle";
+
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           Toast.makeText(MainActivity.this, "MyButton2", Toast.LENGTH_SHORT).show();
+                                       }
+                                   }
+        );
     }
 
     @Override
@@ -47,6 +60,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.v(TAG, "onDestroy()");
+    }
+
+    public void botonClick(View v) {
+        Toast.makeText(this, "MyButton1", Toast.LENGTH_SHORT).show();
+        int i = 10;
+//        i = 1/0;
     }
 
 }
